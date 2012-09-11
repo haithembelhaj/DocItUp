ac0d665693a221ad8cd149bc5ff04382271e614d
haithem <contact@ambitiouslion.com>
2012-09-11 11:41:51 +0200

    added better linking

--- docItUp.py
+++ docItUp.py
@@ -32,17 +32,6 @@ def postprocessor(html, filename):
     return html
 
 
-def getFiles():
-    links = ""
-    global linksHtml
-    for dirname, dirnames, filenames in os.walk(build_path):
-        for filename in filenames:
-            if filename[-3:] == ".md":
-                files.append(open(os.path.join(dirname, filename), 'r'))
-                links += '+ [%s](%s)\n' % (filename, filename[:-3] + ".html")
-    linksHtml = markdown2.markdown(links, extras=['footnotes', 'toc'])
-
-
 def convertmd(f):
     md = f.read()
     # convert the markdown
@@ -51,7 +40,7 @@ def convertmd(f):
     return postprocessor(markdown_html, os.path.basename(f.name))
 
 
-def generateHtml(html):
+def generateHtml(html, links=""):
     html_contents = u'<!DOCTYPE html>'
     html_contents += '<html><head><meta charset="utf-8">'
     styles = getCSS()
@@ -63,26 +52,46 @@ def generateHtml(html):
     html_contents += '$(document).ready(function(){$("pre code").each(function(i, e) {hljs.highlightBlock(e)});});'
     html_contents += '</script>'
     html_contents += u'</head><body><div id="jump_to">         Jump To …         <div id="jump_wrapper"><div id="jump_page">'
-    html_contents += linksHtml
+    html_contents += markdown2.markdown(links, extras=['footnotes', 'toc'])
     html_contents += '</div></div></div>'
     html_contents += html
     html_contents += '</body>'
     return html_contents.encode('utf-8')
 
 
-def convertFiles():
-    for f in files:
-        html = generateHtml(convertmd(f))
-        htmlFile = open(f.name[:-2] + 'html', 'w')
-        htmlFile.write(html)
-        htmlFile.close()
-        f.close()
+def convertFile(f, links):
+    html = generateHtml(convertmd(f), links)
+    htmlFile = open(f.name[:-2] + 'html', 'w')
+    htmlFile.write(html)
+    htmlFile.close()
+    f.close()
 
 
-def buildIndex():
-    index = open(build_path + "/index.html", "w")
-    index.write(generateHtml('<h1>' + settings['project_name'] + '</h1>' + linksHtml))
+def buildIndex(dirname, links):
+    index = open(dirname + "/index.html", "w")
+    linksHtml = markdown2.markdown(links, extras=['footnotes', 'toc'])
+    if dirname == build_path:
+        title = settings['project_name']
+    else:
+        title = os.path.basename(dirname)
+    index.write(generateHtml('<h1>' + title + '</h1>' + linksHtml))
     index.close()
+
+
+def convertFiles():
+    global linksHtml
+    for dirname, dirnames, filenames in os.walk(build_path):
+        links = ""
+        for d in dirnames:
+            links += '+ [%s](%s)\n' % (d, d + "/index.html")
+        for filename in filenames:
+            if filename[-3:] == ".md":
+                links += '+ [%s](%s)\n' % (filename, filename[:-3] + ".html")
+        for filename in filenames:
+            if filename[-3:] == ".md":
+                convertFile(open(os.path.join(dirname, filename), 'r'), links)
+
+        buildIndex(dirname, links)
 
 
 if __name__ == "__main__":
@@ -90,6 +99,4 @@ if __name__ == "__main__":
     settings = json.loads(settingFile.read())
     settingFile.close()
     build_path = settings['build_path']
-    getFiles()
     convertFiles()
-    buildIndex()
